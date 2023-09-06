7c61da8c65200924276c26d8857ea2c23da286e7
Mahdi Hazrati <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-09-06 20:52:53 +0000

    Config font

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,9 @@
+import "./app.css"
 function App() {
   return (
     <div className="app">
-      <h1>Tehran Malek - React App - inital</h1>
+      <h1>تهران مالک - وب اپ</h1>
+      <p>123456789</p>
     </div>
   );
 }
